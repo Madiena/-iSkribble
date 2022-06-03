d2201ce9e950140e902fef03d41d09872322b733
Madelaine Hestermann <madelaine.hestermann@mni.thm.de>
2022-06-03 17:55:22 +0200

    Add View to project

---
+++ backend/Sources/App/View/DrawingApp/DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by Lothar Hestermann on 30.05.22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
