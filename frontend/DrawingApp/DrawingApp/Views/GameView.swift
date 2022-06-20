cbd87c510e37937865a6d68cb4adb4533bc57cf5
Maik Mursall <maik.mursall@mni.thm.de>
2022-06-20 14:01:32 +0200

    Moved currentDrawing, color and lineWidt to GameManager

--- frontend/DrawingApp/DrawingApp/Views/GameView.swift
+++ frontend/DrawingApp/DrawingApp/Views/GameView.swift
@@ -12,18 +12,11 @@ struct GameView: View {
     @EnvironmentObject var gameManager: GameManager
     
     @State var drawingControlShown: Bool = true
-    @State private var currentDrawing: Drawing = Drawing(color:Color.black, lineWidth: 3.0)
-    @State private var color: Color = Color.black
-    @State private var lineWidth: CGFloat = 3.0
     
     var body: some View {
         VStack(alignment: .center) {
             ZStack {
-                DrawingPad(
-                    currentDrawing: $currentDrawing,
-                    color: $color,
-                    lineWidth: $lineWidth
-                )
+                DrawingPad()
                 .frame(width: 310, height: 310, alignment: .center)
 //                DrawingControlsButtonView(drawingControlsShown: drawingControlShown)
 //                    .offset(x: -120, y: 70)
