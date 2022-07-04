23948c6742e78857fcd131d7c8a0a05c6f7048bb
Maik Mursall <maik.mursall@mni.thm.de>
2022-07-04 13:49:25 +0200

    Added .env options for PORT and HOSTNAME

--- backend/Sources/App/configure.swift
+++ backend/Sources/App/configure.swift
@@ -6,6 +6,14 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    if let port = Environment.get("PORT") {
+        app.http.server.configuration.port = Int(port) ?? 8080
+    }
+
+    if let hostname = Environment.get("HOSTNAME") {
+        app.http.server.configuration.hostname = hostname
+    }
+
     // Initialize words list.
     try WordsProvider.shared.loadWords(app: app)
 
